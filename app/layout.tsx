cdb62764dcfbfa9be5543099e00cdee8d80b3a44
Arpit Namdev <arpit8392@gmail.com>
2023-06-06 11:49:53 +0530

    Fix Background Color

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body className={`${poppins.className} bg-lightGrey`}>{children}</body>
+			<body className={`${poppins.className} bg-offWhite`}>{children}</body>
 		</html>
 	)
 }
--- utils/validationSchema.ts
+++ utils/validationSchema.ts
@@ -19,38 +19,34 @@ const isDateValid = (
 export const validationSchema = yup
 	.object()
 	.shape({
+		day: yup
+			.number()
+			.typeError('Must be a valid day')
+			.required('This field is required')
+			.positive('Day must be a positive number')
+			.integer('Day must be an integer')
+			.min(1, 'Must be a valid day')
+			.max(31, 'Must be a valid day'),
 		month: yup
 			.number()
-			.transform((value) => (isNaN(value) ? undefined : value))
-			.required('This field is required')
 			.typeError('Must be a valid month')
+			.required('This field is required')
 			.positive('Month must be a positive number')
 			.integer('Month must be an integer')
 			.min(1, 'Must be a valid month')
 			.max(12, 'Must be a valid month'),
 		year: yup
 			.number()
-			.transform((value) => (isNaN(value) ? undefined : value))
-			.required('This field is required')
 			.typeError('Must be a valid year')
+			.required('This field is required')
 			.positive('Year must be a positive number')
 			.integer('Year must be an integer')
 			.min(1900, 'Year must be greater than or equal to 1900')
 			.max(new Date().getFullYear(), 'Must be in the past'),
-		day: yup
-			.number()
-			.transform((value) => (isNaN(value) ? undefined : value))
-			.required('This field is required')
-			.typeError('Must be a valid day')
-			.positive('Day must be a positive number')
-			.integer('Day must be an integer')
-			.min(1, 'Must be a valid day')
-			.max(31, 'Must be a valid day'),
 	})
 	.required()
 	.test('valid-date', 'Must be a valid date', function (value) {
 		const { day, month, year } = value
 		const result = isDateValid(day, month, year)
-		console.log(result)
 		return result
 	})
